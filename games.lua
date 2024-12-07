8c4d4f7e096652b45665b1196b55acc2508e5edf
slavegod <theokrator@proton.me>
2024-12-06 16:13:56 -0800

    Fix games.lua

--- games.lua
+++ games.lua
@@ -1,3 +1,5 @@
-local games = {
+local game_dictionary = {
 	[9391468976] = "https://raw.githubusercontent.com/slavegod/lazarus/refs/heads/main/scripts/jujutsu%20shenanigans.lua", -- jujutsu shenanigans
 }
+
+return game_dictionary
